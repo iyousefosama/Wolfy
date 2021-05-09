10d59bf95ed53bfb95549bac2a4a68e492630090
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-05-09 13:00:37 +0200

    Update shutdown.js

--- commands/developer/shutdown.js
+++ commands/developer/shutdown.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 
 module.exports.run = async (Client, message, args, prefix) => {
     if(!message.content.startsWith(prefix)) return;
-    if (message.author != '742682490216644619') return
+    if (message.author != '724580315481243668') return
 
     await message.reply("Shutting down now...")
   
