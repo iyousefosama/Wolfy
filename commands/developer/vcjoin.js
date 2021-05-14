a2c72d88052cfd8c070fd3185df7b373cfe11490
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-05-14 18:28:30 +0200

    Update vcjoin.js

--- commands/developer/vcjoin.js
+++ commands/developer/vcjoin.js
@@ -3,7 +3,7 @@ const discord = require('discord.js');
 module.exports.run = async (Client, message, args, prefix) => {
     if(!message.content.startsWith(prefix)) return;
     if(message.author.bot) return;
-    if(message.author.id !== '821655420410003497') return;
+    if(message.author.id !== '724580315481243668') return;
 
           if (message.member.voice.channel) {
             const connection = await message.member.voice.channel.join();
