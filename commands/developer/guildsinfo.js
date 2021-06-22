4d8fc7a8942f567f8fb13eb65136940090266ab9
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-06-22 16:09:38 +0200

    Add files via upload

--- commands/developer/guildsinfo.js
+++ commands/developer/guildsinfo.js
@@ -5,7 +5,8 @@ module.exports.run = async (Client, message, args, prefix) => {
     if(message.author.bot) return;
     if(message.author.id !== '829819269806030879') return;
     Client.guilds.cache.forEach(r => 
-        message.channel.send(`**Server ID:** \`${r.id}\`\n:id: **ServerName:** \`${r.name}\`\n:busts_in_silhouette: **ServerMembers:** \`${r.memberCount}\`\n:date: **ServerCreatedAt:** \`${r.createdAt.toLocaleString()}\`\n:earth_africa: **ServerRegion:** \`${r.region}\`\n:bar_chart: **ServerVerificationLevel:** \`${r.verificationLevel}\`\n:frame_photo: **ServerIcon:** ${r.iconURL()})
+        message.channel.send(`**Server ID:** \`${r.id}\`\n:id: **ServerName:** \`${r.name}\`\n:busts_in_silhouette: **ServerMembers:** \`${r.memberCount}\`\n:date: **ServerCreatedAt:** \`${r.createdAt.toLocaleString()}\`\n:earth_africa: **ServerRegion:** \`${r.region}\`\n:bar_chart: **ServerVerificationLevel:** \`${r.verificationLevel}\`\n:frame_photo: **ServerIcon:** ${r.iconURL()}`))
+
 }
 
     
