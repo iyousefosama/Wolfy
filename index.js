abf92e639dea763a28afd39bf76b6dba5d6f8815
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-08-11 01:04:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ Client.on("guildDelete", guild => {
 })
 
 Client.on('messageDelete', message => {
-    if (message.author.bot) return;
+    if (message.bot) return;
     if (message.channel.type === "dm") return;
     snipes.set(message.channel.id, message)
 
