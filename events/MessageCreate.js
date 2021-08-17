a2ad682682f6186327675f3abb8af544fcd2b267
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-08-17 17:35:59 +0200

    Add files via upload

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -5,6 +5,10 @@ module.exports = {
     name: 'message',
     execute(client, message) {
         if(!message.guild || message.author.bot) return;
+        if (message.author == client.user) return;
+        if (message.author.bot){
+            return;
+          };
         if(!message.content.startsWith(prefix)) return
         console.log(`${message.author.tag} in #${message.channel.name} sent: ${message.content}`);
     }
