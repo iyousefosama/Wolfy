4551fa15ee158ceff134577f5dbf1f7f5f523c19
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-08-17 17:19:12 +0200

    Update MessageCreate.js

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js')
+const { prefix } = require('../../config.json');
 
 module.exports = {
     name: 'message',
     execute(client, message) {
         if(!message.guild || message.author.bot) return;
-        const prefix = client.config.prefix;
         if(!message.content.startsWith(prefix)) return
         console.log(`${message.author.tag} in #${message.channel.name} sent: ${message.content}`);
     }
