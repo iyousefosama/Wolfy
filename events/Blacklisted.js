bc3d3e2d5e329c9611742e5e3f16b0ede0fb125d
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-08-17 17:15:12 +0200

    Add files via upload

---
+++ events/Blacklisted.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const userSchema = require('../schema/user-schema')
+
+module.exports = {
+    name: 'message',
+    async execute(client, message) {
+        let UserData;
+        try {
+            UserData = await userSchema.findOne({
+                userId: message.author.id
+            })
+            if(!UserData) {
+                UserData = await userSchema.create({
+                    userId: message.author.id
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        if(UserData.blacklisted == true) return //message.channel.send("you're blacklisted")
+    }
+} 
