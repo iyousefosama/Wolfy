2ca8f57aac6f186bdcd22af74790ac71c5af4d7d
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-08-18 08:24:34 +0200

    Add files via upload

---
+++ events/ChatBot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const fetch = require('node-fetch')
+
+module.exports = {
+    name: 'message',
+    execute(client, message) {
+        if(message.channel.id === '814692976461611018') {
+            if (message.author == client.user) return;
+            if (message.author.bot){
+                return;
+              };
+            message.channel.startTyping()
+            fetch.default(`https://api.monkedev.com/fun/chat?msg=${message.content}&uid=${message.author.id}`)
+            .then(res => res.json())
+            .then(data => {
+                message.lineReply(data.response, true);
+                message.channel.stopTyping()
+            })
+            .catch(err => {
+                message.lineReply(`Sorry, i can\'t reply this message!`, false);
+                message.channel.stopTyping()
+              })
+    }
+}
+}
---
+++ events/GuildCreate.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const config = require('../config.json')
+
+module.exports = {
+    name: 'guildCreate',
+    async execute(client, guild, guildCreate) {
+        const join = new Discord.MessageEmbed()
+        .setTitle(`${client.user.username} added to a new server!`)
+        .setColor("GREEN")
+        .setThumbnail(guild.iconURL({dynamic: true, format: 'png', size: 512}))
+        .setDescription(`<a:pp224:853495450111967253> Server Name:\n\`\`\`${guild.name}\`\`\` \n<:pp833:853495153280155668> MembersCount:\n\`\`\`${guild.memberCount}\`\`\`\n\n<a:pp330:853495519455215627> Total servers: \`\`\`\n${client.guilds.cache.size}\`\`\`\n<a:pp330:853495519455215627> Total users: \n\`\`\`${client.users.cache.size}\`\`\``)
+        .setTimestamp()
+        const bot = client.user.username;
+        await client.channels.cache.get(config.debug)?.createWebhook(bot, {
+            avatar: client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 128 })
+          })
+          .then(webhook => Promise.all([webhook.send({ embeds: [join] }), webhook]))
+          .then(([_, webhook]) => webhook.delete())
+          .catch(() => {});
+        
+          // add more functions on ready  event callback function...
+        
+          return;
+    }
+}
