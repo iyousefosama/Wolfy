05aabb3a4c9942daa2cfe8c7842281516cea0a91
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-08-19 10:12:49 +0200

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,13 +1,10 @@
 const Discord = require('discord.js')
-const snipes = new Discord.Collection()
 const config = require('../config.json')
-const tag = require('../index.js')
 
 module.exports = {
     name: 'messageDelete',
     async execute(client, message, messageDelete) {
         if (message.author.client) return;
-        snipes.set(message.channel.id, message)
         
         if (!message.author) return;
         const DeletedLog = new Discord.MessageEmbed()
