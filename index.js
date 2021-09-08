4cadf9e346620d88c22aac811033c6e49605b86c
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-09-08 21:35:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const config = require('./config.json');
 // create a new Discord client 
 const client = new Client({
      partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_VOICE_STATES],
+     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_BANS],
      allowedMentions: { parse: ['users', 'roles'], repliedUser: true }
     });
 
