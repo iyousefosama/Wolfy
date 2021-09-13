cf8834470059a7c6d004dbe4327021abb8d1a009
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-09-13 05:34:24 +0200

    Update Economy-Schema.js

--- schema/Economy-Schema.js
+++ schema/Economy-Schema.js
@@ -9,7 +9,26 @@ const ecoSchema = mongoose.Schema({
     credits: {
         type: Number,
         default: 275
-    }
+    },
+
+    timer: {
+        beg: {
+          timeout: { type: Date, default: null }
+        },
+        daily: {
+            timeout: { type: Date, default: null }
+          }
+      },
+
+    cookies: {
+        totalcookies: { type: Number, default: 0 },
+        givecookies: { type: Number, default: 0 }
+      },
+
+      inv: {
+        FishinPole: { type: Number, default: 0 },
+        UltimateCookie: { type: Number, default: 0 }
+      }
 })
 
 module.exports = mongoose.model('Economy', ecoSchema)
