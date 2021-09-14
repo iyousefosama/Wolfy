a2fbd2eb6e991d61505a6b6781654dbe65b40a79
iiBlackwolf <78449359+iiBlackwolf@users.noreply.github.com>
2021-09-15 01:20:49 +0200

    Add files via upload

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -1,14 +1,39 @@
 const Discord = require('discord.js')
-const { prefix } = require('.././config.json');
+const schema = require('../schema/GuildSchema')
+const config = require('../config.json')
 
 module.exports = {
     name: 'messageCreate',
-    execute(client, message) {
+    async execute(client, message) {
         if(!message.guild || message.author.bot) return;
         if (message.author == client.user) return;
         if (message.author.bot){
             return;
           };
+          let data;
+          let prefix;
+          if (message.guild) {
+          try{
+              data = await schema.findOne({
+                  GuildID: message.guild.id
+              })
+          } catch(err) {
+              console.log(err)
+          }
+      }
+      if (message.content.startsWith('wolfy ')){
+        prefix = 'wolfy '
+      } else if(message.channel.type === 'DM') {
+        prefix = config.prefix;
+      } else if (!data || data.prefix == null){
+        prefix = config.prefix;
+      } else if (data && message.content.startsWith(data.prefix)){
+        prefix = data.prefix;
+      };
+    
+      if (!prefix){
+        return { executed: false, reason: 'PREFIX'};
+      };
         if(!message.content.startsWith(prefix)) return
         console.log(`${message.author.tag}|(${message.author.id}) in #${message.channel.name}|(${message.channel.id}) sent: ${message.content}`);
     }
--- events/botmention.js
+++ events/botmention.js
@@ -1,9 +1,22 @@
 const Discord = require('discord.js')
-const { prefix } = require('.././config.json');
+const schema = require('../schema/GuildSchema')
+const config = require('../config.json')
 
 module.exports = {
     name: 'messageCreate',
-    execute(client, message) {
+    async execute(client, message) {
+
+        let data;
+        let prefix;
+        if (message.guild) {
+        try{
+            data = await schema.findOne({
+                GuildID: message.guild.id
+            })
+        } catch(err) {
+            console.log(err)
+        }
+    }
         if(!message.guild || message.author.bot) return;
         if (message.author == client.user) return;
         if (message.author.bot){
@@ -12,7 +25,7 @@ module.exports = {
           if (message.content.includes("@here") || message.content.includes("@everyone")) return;
           if (message.mentions.has(client.user)) {
             setTimeout(async function(){
-            message.reply({ content: `<a:Right:877975111846731847> Hello **${message.author.username}**, my prefix is \`${prefix}\` for the helplist \`${prefix}help\`!` })
+            message.reply({ content: `<a:Right:877975111846731847> Hello **${message.author.username}**, my prefix is \`${config.prefix}\` my custom guild prefix \`${data.prefix || "None"}\` for the helplist \`${prefix}help\`!` })
         }, 900);
         }
     }
