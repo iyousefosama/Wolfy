003e199f1065978e7b866fb67c9b31ec840fe7d0
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2021-09-17 08:46:23 +0200

    Add files via upload

--- commands/Search/lyrics.js
+++ commands/Search/lyrics.js
@@ -1,6 +1,7 @@
 const discord = require('discord.js')
 const lyricsFinder = require("lyrics-finder")
 const { prefix } = require('../../config.json');
+const fetch = require('node-fetch');
 
 module.exports = {
     name: "lyrics",
@@ -9,7 +10,7 @@ module.exports = {
     guildOnly: true, //or false
     args: false, //or false
     usage: '',
-    cooldown: 2, //seconds(s)
+    cooldown: 3, //seconds(s)
     guarded: false, //or false
     clientpermissions: ["USE_EXTERNAL_EMOJIS"],
     async execute(client, message, args) {
@@ -21,47 +22,64 @@ module.exports = {
 
     const filter = msg => msg.author.id == message.author.id;
 
-    let mentionedMember = message.member;
     let singerEmb = new discord.MessageEmbed()
-    .setColor(`GOLD`)
-    .setDescription(`<a:Loading:841321898302373909> **Who is the singer?**`)
-    .setFooter(`1/2`)
+    .setAuthor(message.author.username, message.author.displayAvatarURL({dynamic: true, size: 2048}))
+    .setColor(`#d6a565`)
+    .setDescription(`<a:Loading:841321898302373909> | Please send the **artist** name!`)
+    .setFooter(`1/2 | type "cancel" to cancel the command`, message.author.displayAvatarURL({dynamic: true, size: 2048}))
     message.channel.send({ embeds: [singerEmb] })
-    let col = await message.channel.awaitMessages({ filter, max: 1 })
-    if(col.first().content == 'cancel') return message.channel.send({ content: "<a:pp681:774089750373597185> **Cancelled!**"});
-    else if(col.first().content == `${prefix}lyrics`) return message.channel.send({ content: "<a:pp681:774089750373597185> **Cancelled!**"})
+    let col = await message.channel.awaitMessages({ filter, max: 1, time: 5000 })
+    if(col.first().content == 'cancel') return message.channel.send({ content: `<:error:888264104081522698>  **|** **${message.author.tag}**, Cancelled the \`lyrics\` command!`});
+    else if(col.first().content == `${prefix}lyrics`) return message.channel.send({ content: `<:error:888264104081522698>  **|** **${message.author.tag}**, Cancelled the \`lyrics\` command!`})
     singer = col.first().content
 
     let songEmb = new discord.MessageEmbed()
-    .setColor(`GREEN`)
-    .setDescription(`<a:Loading:841321898302373909> **What is the name of the song?**`)
-    .setFooter(`2/2`)
+    .setColor(`#98ff98`)
+    .setAuthor(message.author.username, message.author.displayAvatarURL({dynamic: true, size: 2048}))
+    .setDescription(`<a:Loading:841321898302373909> | Please send the **song** name!`)
+    .setFooter(`2/2 | type "cancel" to cancel the command`, message.author.displayAvatarURL({dynamic: true, size: 2048}))
+    .setTimestamp()
     message.channel.send({ embeds: [songEmb]})
-    let col2 = await message.channel.awaitMessages({ filter, max: 1 })
-    if(col2.first().content == 'cancel') return message.channel.send({ content: "<a:pp681:774089750373597185> **Cancelled!**"});
-    else if(col2.first().content == `${prefix}lyrics`) return message.channel.send({ content: "<a:pp681:774089750373597185> **Cancelled!**"})
+    let col2 = await message.channel.awaitMessages({ filter, max: 1, time: 5000 })
+    if(col2.first().content == 'cancel') return message.channel.send({ content: `<:error:888264104081522698>  **|** **${message.author.tag}**, Cancelled the \`lyrics\` command!`});
+    else if(col2.first().content == `${prefix}lyrics`) return message.channel.send({ content: `<:error:888264104081522698>  **|** **${message.author.tag}**, Cancelled the \`lyrics\` command!`})
     song = col2.first().content
 
-    let res = await lyricsFinder(singer, song) || "<a:Error:836169051310260265> **|** Not Found!";
+    const data = await fetch(`https://some-random-api.ml/lyrics?title=${encodeURI(song)}`)
+    .then(res => res.json())
+    .catch(() => null);
+
+    if (!data || data.error){
+        return message.channel.send(`\\\❌ | ${message.author}, I couldn't find the lyrics!`)
+      };
+
+    let res = await lyricsFinder(singer, song) || "<a:Error:836169051310260265> | Not Found!";
 
     for(let i = 0; i < res.length; i += 2048) {
         let lyrics = res.substring(i, Math.min(res.length, i + 2048))
         let page = new discord.MessageEmbed()
-        .setAuthor(`${mentionedMember.user.username}`, mentionedMember.user.displayAvatarURL({dynamic: true, size: 2048}))
+        .setThumbnail(data.thumbnail.genius)
+        .setAuthor(`${data.title}\n${data.author}`, null, data.links.genius)
+        .setColor('GREY')
         .addFields(
-            { name: '<:pp421:853495091338674206> Singer', value: `\`\`\`${singer}\`\`\``, inline: true },
-            { name: '<:pp421:853495091338674206> Song', value: `\`\`\`${song}\`\`\``, inline: true },
+            { name: '<:pp421:853495091338674206> Singer', value: `\`\`\`${data.title}\`\`\``, inline: true },
+            { name: '<:pp421:853495091338674206> Song', value: `\`\`\`${data.author}\`\`\``, inline: true },
         )
         .setDescription(lyrics)
         pages.push(page)
     }
 
-    const filter2 = (reaction, user) => ['⬅️', '➡️'].includes(reaction.emoji.name) && (message.author.id == user.id)
+    const filter2 = (reaction, user) => ['⬅️', '➡️', '❌'].includes(reaction.emoji.name) && (message.author.id == user.id)
     const Embed = await message.channel.send({ content: `<:pp332:853495194863534081> **Page:** \`${current+1}/${pages.length}\``, embeds: [pages[current]]})
     await Embed.react('⬅️')
+    setTimeout(async () => {
     await Embed.react('➡️')
+    }, 1000)
+    setTimeout(async () => {
+    await Embed.react('❌')
+    }, 1000)
 
-    let ReactionCol = Embed.createReactionCollector({ filter: filter2})
+    let ReactionCol = Embed.createReactionCollector({ filter: filter2, time: 30000 })
 
     ReactionCol.on("collect", (reaction, user) => {
         reaction.users.remove(message.author.id)
@@ -77,8 +95,11 @@ module.exports = {
                     current -= 1
                     Embed.edit({ content: `<:pp332:853495194863534081> **Page:** \`${current+1}/${pages.length}\``, embeds: [pages[current]]})
                 }
+            } else if(reaction.emoji.name == '❌') {
+                Embed.reactions.removeAll().catch(()=>null)
             }
         }
     })
+    ReactionCol.on('end', async () => await Embed.reactions.removeAll().catch(()=>null) ? null : null);
 }
 }
