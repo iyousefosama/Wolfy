b70c4d58d08a146f4102ff87b6b0db0c02acca5e
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2021-11-23 14:36:50 +0200

    Update ChatBot.js

--- events/ChatBot.js
+++ events/ChatBot.js
@@ -4,20 +4,23 @@ const fetch = require('node-fetch')
 module.exports = {
     name: 'messageCreate',
     execute(client, message) {
+        if (message.channel.type === 'DM') return;
+        if (message.author == client.user) return;
+        if (message.author.bot){
+            return;
+          };
+        if (!message.author) return;
+        if(message.embeds[0]) return;
+        if(message.attachments.size) return;
         if(message.channel.id === '911566889849876512') {
-            if (message.author == client.user) return;
-            if (message.author.bot){
-                return;
-              };
             message.channel.sendTyping()
             fetch.default(`https://api.monkedev.com/fun/chat?msg=${message.content}&uid=${message.author.id}`)
             .then(res => res.json()).catch(() => null)
             .then(data => {
-                message.reply({ content: data.response, allowedMentions: { repliedUser: true }})
-            }).catch(() => null)
+                message.reply({ content: data.response || 'Sorry, i can\'t reply this message!', allowedMentions: { repliedUser: true }})
+            })
             .catch(err => {
-
-                message.reply({ content: 'Sorry, i can\'t reply this message!', allowedMentions: { repliedUser: true }})
+                return message.reply({ content: 'Sorry, i can\'t reply this message!', allowedMentions: { repliedUser: true }})
               })
     }
 }
