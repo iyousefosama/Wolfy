c196f00874c15147297e09d1d91ab1886ca9b2d4
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2021-11-27 02:45:50 +0200

    Add files via upload

--- commands/Economy/sell.js
+++ commands/Economy/sell.js
@@ -7,7 +7,7 @@ module.exports = {
     dmOnly: false, //or false
     guildOnly: true, //or false
     args: true, //or false
-    usage: '',
+    usage: '[item] <amount>',
     group: 'Economy',
     description: 'Sell item from your inventory and get some credits!',
     cooldown: 5, //seconds(s)
@@ -19,11 +19,13 @@ module.exports = {
       ],
     async execute(client, message, [item = '', ...amount]) {
 
-        if(!item) return message.channel.send({ content: `\\❌ **${message.member.displayName}**, Please provide a valid item.` })
-
-        if(!amount) return message.channel.send({ content: `\\❌ **${message.member.displayName}**, please provide a valid credits number.` })
-        if(isNaN(amount)) return message.channel.send({ content: `\\❌ **${message.member.displayName}**, please provide a valid credits number.` })
-        if(!amount || amount === 'Nothing') return message.channel.send(`\\❌ **${message.author.tag}**, \`${amount}\` is not a valid amount!`);
+        if(!item) {
+            return message.channel.send({ content: `\\❌ **${message.member.displayName}**, Please provide a valid item.` })
+        } else if(isNaN(amount)) {
+            return message.channel.send({ content: `\\❌ **${message.member.displayName}**, please provide a valid item amount.` })
+        } else if(!amount || amount == 0) {
+            return message.channel.send({ content: `\\❌ **${message.member.displayName}**, please provide a valid item amount.` })
+        }
 
         let data;
         try{
@@ -39,48 +41,51 @@ module.exports = {
             console.log(err)
         }
         let res = item.toLowerCase();
+        const Coal_price = Math.floor(12)
+        const Stone_price = Math.floor(5)
+        const Iron_price = Math.floor(25)
+        const gold_price = Math.floor(75)
+        const Diamond_price = Math.floor()
         if(res === 'coal') {
-            if(data.inv.Coal < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, Insuffecient credits! You only have **${data.credits}** in your wallet!`})
-            const finall = Math.floor(amount * 2)
+            if(data.inv.Coal < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, You only have **${data.inv.Coal}** in your inventory!`})
+            const finall = Math.floor(Coal_price * 0.7 * amount)
             data.credits += finall
             data.inv.Coal -= Math.floor(amount)
             await data.save()
-            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<:e_:887034070842900552> Coal** for <a:ShinyMoney:877975108038324224> \`+${amount * 2}\`!`))
+            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<:e_:887034070842900552> Coal** for <a:ShinyMoney:877975108038324224> \`+${finall}\`!`))
             .catch(err => message.channel.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: \`${err.name}\``));
         } else if(res === 'stone') {
-            if(data.inv.Stone < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, Insuffecient credits! You only have **${data.credits}** in your wallet!`})
-            const finall = Math.floor(amount * 1)
+            if(data.inv.Stone < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, You only have **${data.inv.Stone}** in your inventory!`})
+            const finall = Math.floor(Stone_price * 0.7 * amount)
             data.credits += finall
             data.inv.Stone -= Math.floor(amount)
             await data.save()
-            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<:e_:887031111790764092> Stone** for <a:ShinyMoney:877975108038324224> \`+${amount * 1}\`!`))
+            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<:e_:887031111790764092> Stone** for <a:ShinyMoney:877975108038324224> \`+${finall}\`!`))
             .catch(err => message.channel.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: \`${err.name}\``));
         } else if(res === 'iron') {
-            if(data.inv.Iron < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, Insuffecient credits! You only have **${data.credits}** in your wallet!`})
-            const finall = Math.floor(amount * 4)
+            if(data.inv.Iron < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, You only have **${data.inv.Iron}** in your inventory!`})
+            const finall = Math.floor(Iron_price * 0.7 * amount)
             data.credits += finall
             data.inv.Iron -= Math.floor(amount)
             await data.save()
-            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<:e_:887034687472689192> Iron** for <a:ShinyMoney:877975108038324224> \`+${amount * 3}\`!`))
+            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<:e_:887034687472689192> Iron** for <a:ShinyMoney:877975108038324224> \`+${finall}\`!`))
             .catch(err => message.channel.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: \`${err.name}\``));
         } else if(res === 'gold') {
-            if(data.inv.Gold < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, Insuffecient credits! You only have **${data.credits}** in your wallet!`})
-            const finall = Math.floor(amount * 45)
+            if(data.inv.Gold < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, You only have **${data.inv.Gold}** in your inventory!`})
+            const finall = Math.floor(gold_price * 0.7 * amount)
             data.credits += finall
             data.inv.Gold -= Math.floor(amount)
             await data.save()
-            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<:e_:887036608874967121> Gold** for <a:ShinyMoney:877975108038324224> \`+${amount * 4}\`!`))
+            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<:e_:887036608874967121> Gold** for <a:ShinyMoney:877975108038324224> \`+${finall}\`!`))
             .catch(err => message.channel.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: \`${err.name}\``));
         } else if(res === 'diamond') {
-            if(data.inv.Diamond < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, Insuffecient credits! You only have **${data.credits}** in your wallet!`})
-            const finall = Math.floor(amount * 115)
+            if(data.inv.Diamond < amount) return message.channel.send({ content: `\\❌ **${message.author.tag}**, You only have **${data.inv.Diamond}** in your inventory!`})
+            const finall = Math.floor(Diamond_price * 0.7 * amount)
             data.credits += finall
             data.inv.Diamond -= Math.floor(amount)
             await data.save()
-            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<a:Diamond:877975082868301824> Diamond** for <a:ShinyMoney:877975108038324224> \`+${amount * 6}\`!`))
+            .then(()=> message.channel.send(`\\✔️ **${message.author.tag}**, Successfully sold **<a:Diamond:877975082868301824> Diamond** for <a:ShinyMoney:877975108038324224> \`+${finall}\`!`))
             .catch(err => message.channel.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: \`${err.name}\``));
-        } else if(!amount || isNaN(amount)) {
-            return message.channel.send(`\\❌ **${message.author.tag}**, Please type a valid amount!.`);
         } else {
             const nulle = new Discord.MessageEmbed()
             .setTitle(`<a:Wrong:812104211361693696> Unknown item!`)
