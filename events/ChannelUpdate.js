2161a3ff4518219de9883ad666912b60c24f2af2
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2021-11-30 17:08:49 +0200

    Add files via upload

--- events/ChannelUpdate.js
+++ events/ChannelUpdate.js
@@ -82,7 +82,7 @@ module.exports = {
             .setFooter(oldChannel.guild.name, oldChannel.guild.iconURL({dynamic: true}))
             .setTimestamp()
           } else if(oldChannel.permissions !== newChannel.permissions) {
-            RoleUpdated = new MessageEmbed()
+            ChannelDeleted = new MessageEmbed()
             .setAuthor(executor.username, executor.displayAvatarURL({dynamic: true, size: 2048}))
             .setTitle('<a:Mod:853496185443319809> Channel permissions Updated!')
             .setDescription(`<a:Right:860969895779893248> **Channel:** ${newChannel.name} (\`${newChannel.id}\`)\n<:Rules:853495279339569182> **ExecutorTag:** ${executor.tag}\n\n<a:Right:877975111846731847> **Old Permissions:**\n\`\`\`\n${text.joinArray(oldChannel.permissions.toArray())}\`\`\`\n<a:Right:877975111846731847> **New Permissions:**\n\`\`\`\n${text.joinArray(newChannel.permissions.toArray())}\`\`\``)
