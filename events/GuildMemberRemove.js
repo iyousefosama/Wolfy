27924a163994a1578321b554556a03e996743bd1
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2021-12-21 00:49:30 +0200

    Add files via upload

--- events/GuildMemberRemove.js
+++ events/GuildMemberRemove.js
@@ -5,7 +5,12 @@ const schema = require('../schema/GuildSchema')
 module.exports = {
     name: 'guildMemberRemove',
     async execute(client, member) {
-
+      if(!member) {
+        return;
+      } else {
+        // Do nothing..
+      }
+      
         let data;
         try{
             data = await schema.findOne({
@@ -40,15 +45,15 @@ module.exports = {
           const timestamp = Math.floor(Date.now() / 1000)
         
         let RemoveEmbed;
-        if (kickLog || kickLog.available || kickLog.createdAt > member.joinedAt && target.id == member.id) {
+        if (!kickLog || !kickLog.available || kickLog.createdAt < member.joinedAt) {
           RemoveEmbed = new Discord.MessageEmbed()
-          .setAuthor(target.username, target.displayAvatarURL({dynamic: true, size: 2048}))
-          .setTitle('<a:Mod:853496185443319809> Member Kicked!')
-          .setDescription(`<:Humans:853495153280155668> **Member:** ${target.tag} (\`${target.id}\`)\n<a:Mod:853496185443319809> **Executor:** ${executor.tag}\n<a:Right:877975111846731847> **At:** <t:${timestamp}>`)
-          .setColor('#e6a54a')
+          .setAuthor(member.user.username, member.user.displayAvatarURL({dynamic: true, size: 2048}))
+          .setTitle('<a:Down:853495989796470815> Member Leave!')
+          .setDescription(`<a:iNFO:853495450111967253> **MemberTag:** ${member.user.tag}\n<:pp198:853494893439352842> **MemberID:** \`${member.user.id}\`\n<a:Right:877975111846731847> **Created At:** ${moment.utc(member.user.createdAt).format('LT')} ${moment.utc(member.user.createdAt).format('LL')} (\`${moment.utc(member.user.createdAt).fromNow()}\`)\n<a:Right:877975111846731847> **Joined At:** ${moment(member.joinedAt).format("LT")} ${moment(member.joinedAt).format('LL')} (\`${moment(member.joinedAt).fromNow()}\`)`)
+          .setColor('#2F3136')
           .setFooter(member.guild.name, member.guild.iconURL({dynamic: true}))
-          .setTimestamp()
-        } else if(!kickLog || !kickLog.available || kickLog.createdAt < member.joinedAt && target.id != member.id) {
+          .setTimestamp() 
+        } else if(kickLog || kickLog.available && target.id == member.id) {
           RemoveEmbed = new Discord.MessageEmbed()
           .setAuthor(member.user.username, member.user.displayAvatarURL({dynamic: true, size: 2048}))
           .setTitle('<a:Down:853495989796470815> Member Leave!')
