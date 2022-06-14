a5ff35bdb3a8407295568ad78535754ffeb598c0
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2022-06-14 04:32:45 +0200

    Update MusicEvents.js

--- MusicEvents.js
+++ MusicEvents.js
@@ -2,10 +2,12 @@ module.exports.registerPlayerEvents = (player) => {
 
     player.on("error", (queue, error) => {
         console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
+        queue.metadata.channel.send(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
     });
 
     player.on("connectionError", (queue, error) => {
         console.log(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
+        queue.metadata.channel.send(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
     });
 
     player.on("trackStart", (queue, track) => {
