ded03c24fb5863fb50762e754feff32ec17b0171
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2022-07-15 03:41:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ const client = new Client({
      allowedMentions: { parse: ['users', 'roles'], repliedUser: true }
     });
 
-// require the fs module
-const fs = require('fs');
-
 const { Player } = require("discord-player")
 
 const { registerPlayerEvents } = require('./events/MusicEvents');
@@ -27,89 +24,16 @@ client.player = new Player(client, {
     }
 });
 
-const fetch = require('node-fetch')
-
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-
-const { developer, clientId } = require('./config.json');
-
-const map = new Map();
-
 const mongodb = require('./mongo')()
-
-client.commands = new Discord.Collection();
-
-client.slashCommands = new Collection();
-
-const commands=[]
 
 registerPlayerEvents(client.player);
 
-const slashFiles = fs.readdirSync('./slashCommands').filter(file => file.endsWith('.js'));
-for (const file of slashFiles) {
-    if (slashFiles.length <= 0) {
-        console.log("(/) Can't find any slash commands!");
-        return;
-      }
-    const slash = require(`./slashCommands/${file}`);
-    client.slashCommands.set(file.split(/.js$/)[0],slash);
-    commands.push(slash.data.toJSON());
-}
+["SlashHandler", "CommandHandler", "EventHandler"].forEach((handler) => {
+	require(`./handler/${handler}`)(client);
+});
 
-
-		//Once the Bot is ready, add all Slas Commands to each guild
-		client.on("ready", async () => {
-			client.user.setPresence({ activities: [{ name: 'Loading...', type: "COMPETING" }], status: 'dnd' });
-			if(config.loadSlashsGlobal){
-				client.application.commands.set(commands)
-				.then(slashCommandsData => {
-					console.log(`(/) ${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`} Loaded for ${`All possible Guilds`}`); 
-				}).catch((e)=>console.log(e));
-			} else {
-				client.guilds.cache.map(g => g).forEach(async (guild) => {
-					try{
-						guild.commands.set(commands)
-						.then(slashCommandsData => {
-							console.log(`(/) ${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`} Loaded for: ${`${guild.name}`}`); 
-						}).catch((e)=>console.log(e))
-					}catch (e){
-						console.log(e)
-					}
-				});
-			}
-		})
-
-const commandFolders = fs.readdirSync('./commands');
-for (const folder of commandFolders) {
-	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const command = require(`./commands/${folder}/${file}`);
-        client.commands.set(command.name, command);
-    } 
-    if (commandFiles.length <= 0) {
-        console.log("Can't find any commands!");
-        return;
-      }
-
-      commandFiles.forEach(cmd => {
-        // console the loaded cmds 
-        var commandFiles = require(`./commands/${folder}/${cmd}`);
-        console.log(`File ${cmd} was loaded`)
-      })
-}
-
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(client, ...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(client, ...args));
-    	}
-    }
-
-// Login To Discord with your app's Token
+["Reminder"].forEach((functions) => {
+	require(`./functions/${functions}`)(client);
+});
 
 client.login(process.env.TOKEN_URI);
