fe4c205d8ed53534f0f1ec7684a3c242ebef8eda
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2022-08-24 03:33:50 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,45 @@
+// NOTE: ./struct not included on test
+
+const options = {
+    paths: [
+      'admin', 'bot', 'developer',
+      'Economy', 'fun', 'information',
+      'LeveledRoles', 'Search', 'setup','Utilities'
+    ]
+  };
+  
+  const { readdir } = require('fs');
+  
+  // Test commands
+  for (const path of options.paths){
+    readdir(`./commands/${path}`, (err, files) => {
+      files.filter(file => file.split('.').pop === 'js').forEach(file => {
+        require(`./commands/${path}/${file}`);
+      });
+    });
+  };
+  
+  // Test events
+  readdir(`./events`, (err, files) => {
+    files.filter(file => file.split('.').pop() === 'js').forEach(file => {
+      require(`./events/${file}`);
+    });
+  });
+  
+  // Test JSON assets
+  readdir('./assets/json', (err, files) => {
+    files.filter(file => file.split('.').pop() === 'json').forEach(file => {
+      require(`./assets/json/${file}`);
+    });
+  });
+  
+  // Test utils
+  readdir('./util', (err, files) => {
+    files.filter(file => file.split('.').pop() === 'js').forEach(file => {
+      require(`./util/${file}`);
+    });
+  });
+  
+  
+  // End of Test
+  
