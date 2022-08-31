e0e2e526bc7ea7207d3417eb6b18ec3cb3180339
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2022-08-31 06:11:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,24 +12,12 @@ client.listentoProcessEvents([
   'uncaughtException'
 ], { ignore: false });
 
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '.env') })
-
-const { Player } = require("discord-player")
-
-const { registerPlayerEvents } = require('./events/MusicEvents');
-
-client.player = new Player(client, client.config.ytdlOptions)
-
-client.database?.init();
-
-registerPlayerEvents(client.player);
 
 ["SlashHandler", "CommandHandler", "EventHandler"].forEach((handler) => {
 	require(`./Handler/${handler}`)(client);
 });
 
-["Reminder", "checkQuests"].forEach((functions) => {
+["Reminder", "checkQuests", "Mudules"].forEach((functions) => {
 	require(`./functions/${functions}`)(client);
 });
 
