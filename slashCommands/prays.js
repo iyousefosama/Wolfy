a2b93c9786e2dee94449d91303729eaa518dcb88
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2022-08-31 06:12:48 +0200

    Add files via upload

--- slashCommands/prays.js
+++ slashCommands/prays.js
@@ -2,6 +2,7 @@ const discord = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fetch = require('node-fetch');
 const tc = require('../functions/TimeConvert')
+const cfl = require('../functions/CapitalizedChar')
 
 module.exports = {
     clientpermissions: ['EMBED_LINKS', 'READ_MESSAGE_HISTORY'],
@@ -26,9 +27,13 @@ module.exports = {
         fetch(url, options)
             .then(res => res.json())
             .then(json => {
+                if(typeof json.data === "string" && json.data.startsWith('Unable to locate city and country')) {
+                  return interaction.editReply({ content: '<:error:888264104081522698> Please enter valid country and city in the options!' });
+                };
+
                 const embed = new discord.MessageEmbed()
                 .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL({ dynamic: true })})
-                .setDescription(`<:Tag:836168214525509653> Prays times for country \`${country}\` in city \`${city}\`!`)
+                .setDescription(`<:Tag:836168214525509653> Prays times for country \`${cfl.capitalizeFirstLetter(country)}\` in city \`${cfl.capitalizeFirstLetter(city)}\`!`)
                 .addFields(
                     { name: 'Date', value: `<t:${json.data.date.timestamp}>`, inline: false},
                     { name: ' ‍ ', value: ` ‍ `, inline: false},
@@ -44,6 +49,6 @@ module.exports = {
                 .setTimestamp()
                 interaction.editReply({ embeds: [embed] });
             })
-            .catch(() => interaction.editReply({ content: '<:error:888264104081522698> Please enter valid country and city in the options!' }));
+            .catch(() => interaction.editReply({ content: '<:error:888264104081522698> Something went wrong, please try again later!' }));
 	},
 };
