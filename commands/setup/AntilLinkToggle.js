54c31f1b12ea65b707672c65f5db982ce8b3d841
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2022-12-22 08:58:43 +0000

    Add files via upload

--- commands/setup/AntilLinkToggle.js
+++ commands/setup/AntilLinkToggle.js
@@ -2,18 +2,21 @@ const Discord = require('discord.js');
 const schema = require('../../schema/GuildSchema')
 
 module.exports = {
-    name: "ticketstoggle",
-    aliases: ["Ticketstoggle", "TicketsToggle", "TICKETSTOGGLE"],
+    name: "antilinktoggle",
+    aliases: ["AntiLinkToggle", "ANTILINKTOGGLE", "Antilinktoggle", "anti-link-toggle"],
     dmOnly: false, //or false
     guildOnly: true, //or false
     args: false, //or false
     usage: '',
     group: 'setup',
-    description: 'To allow or disable ticket feature!',
-    cooldown: 30, //seconds(s)
+    description: 'To enable/disable Anti-Links protection!',
+    cooldown: 5, //seconds(s)
     guarded: false, //or false
-    permissions: ["MANAGE_CHANNELS", "ADMINISTRATOR"],
-    examples: [''],
+    permissions: ["ADMINISTRATOR"],
+    clientpermissions: ["MANAGE_MESSAGES"],
+    examples: [
+    ''
+    ],
     async execute(client, message, args) {
           
         let data;
@@ -21,28 +24,30 @@ module.exports = {
             data = await schema.findOne({
                 GuildID: message.guild.id
             })
-            if(!data.Mod.Tickets.channel || !data) {
-                return message.channel.send(`\\❌ **${message.member.displayName}**, You didn't set tickets channel yet`);
+            if(!data) {
+                data = await schema.create({
+                    GuildID: message.guild.id
+                })
             }
         } catch(err) {
             console.log(err)
             message.channel.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: ${err.name}`)
         }
 
-        data.Mod.Tickets.isEnabled = !data.Mod.Tickets.isEnabled;
+        data.Mod.AntiLink.isEnabled = !data.Mod.AntiLink.isEnabled;
 
         data.save()
         .then(() => {
-          const state = ['Disabled', 'Enabled'][Number(data.Mod.Tickets.isEnabled)];
-          data.Mod.Tickets.isEnabled = data.Mod.Tickets.isEnabled;
+          const state = ['Disabled', 'Enabled'][Number(data.Mod.AntiLink.isEnabled)];
+          data.Mod.AntiLink.isEnabled = data.Mod.AntiLink.isEnabled;
     
           const embed = new Discord.MessageEmbed()
             .setColor('GREEN')
             .setDescription([
               '<a:Correct:812104211386728498>\u2000|\u2000',
-              `Tickets channel Feature has been successfully **${state}**!\n\n`,
-              `To **${!data.Mod.Tickets.isEnabled ? 're-enable' : 'disable'}** this`,
-              `feature, use the \`${client.prefix}ticketstoggle\` command.`
+              `AntiLink protection Feature has been successfully **${state}**!\n\n`,
+              `To **${!data.Mod.AntiLink.isEnabled ? 're-enable' : 'disable'}** this`,
+              `feature, use the \`${client.prefix}antilinktoggle\` command.`
             ].join(' '))
             message.channel.send({ embeds: [embed] })
           }).catch(() => message.channel.send(`\`❌ [DATABASE_ERR]:\` Unable to save the document to the database, please try again later!`));
