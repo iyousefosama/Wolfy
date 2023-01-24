74051b9d6c93219e5c219d857e6d8e0eaf87fd32
iiBlackwolf <blackwolf3466@gmail.com>
2023-01-24 21:49:24 +0200

    Added Client, GatewayIntentBits, Partials objects

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const { Intents } = require('discord.js')
+const { Client, GatewayIntentBits, Partials } = require('discord.js')
 const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
@@ -13,21 +13,21 @@ const settings = {
           type: 'COMPETING'
         }
       },
-      partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+      partials: [Partials.Message, Partials.Channel, Partials.Reaction],
       // Intents flags
-      intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_BANS],
+      intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildBans, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildBans, GatewayIntentBits.MessageContent],
       // Disable Mentions except Users
       allowedMentions: { parse: ['users', 'roles'], repliedUser: true },
     },
 
     // set the default prefix, if non-string data-type is provided, will resolve
     // to the prefix 'w!'
-    prefix: 'w!',
+    prefix: 't!',
   
     // logging channels for the bot. To disable logging specific events - pass
     // a falsy value (undefined, null, 0). You may also remove the property
     // altogether, although this is not preferred.
-    channels: { debug: '840892477614587914', votes: null, chatbot: "911566889849876512" },
+    channels: { debug: '877130715337220136', votes: "840892477614587914", chatbot: "1016840633715007639" },
   
     // enable/disable database system in the bot, this will automatically disable
     // all commands and features that requires database if disabled.
@@ -52,7 +52,7 @@ const settings = {
       }
     },
 
-    loadSlashsGlobal: true,
+    loadSlashsGlobal: false,
     
     // Array of owners recognized by the bot. ID here will be given access to
     // owner based commands.
@@ -61,7 +61,7 @@ const settings = {
     // websites affiliated with the bot, can be accessed through
     // Client#config#websites
     websites: {
-      "website": "https://Wolfy.yoyojoe.repl.co",
+      "website": "https://iiblackwolf.github.io/WolfySite/index.html",
       "invite": "https://discord.com/api/oauth2/authorize?client_id=821655420410003497&permissions=8&scope=bot%20applications.commands",
       "support": "https://discord.gg/qYjus2rujb",
       "top.gg": "https://top.gg/bot/821655420410003497",
