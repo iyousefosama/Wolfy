ed25154c2abc5cfebf369690f6f74de77dd179fd
WOLF <78449359+iiBlackwolf@users.noreply.github.com>
2023-04-03 16:20:03 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -36,7 +36,7 @@ module.exports = {
         ].join('\n'))
         
         function randomStatus() {
-            let status = ["🤖 Wolfy Bot", "🤖 w!help", "🤖 Poob Beep"]
+            let status = ["🤖 Wolfy Bot", "🤖 w!help", "🤖 Poob Beep", "🌙 Ramadan Kareem"]
             let rstatus = Math.floor(Math.random() * status.length);
 
             client.user.setPresence({ activities: [{ name: status[rstatus], type: ActivityType.Playing }], status: 'online' });
