1ebd9edd14aef23bad429be1e9b67c5a80657ffe
BuildTools <unconfigured@null.spigotmc.org>
2024-01-24 18:37:17 +0200

    Express

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const config = require(`${process.cwd()}/config`);
 // create a new Discord client 
 const client = new Client(config);
 
+const express = require('express');
+const server = express();
+
 client.listentoProcessEvents([
   'unhandledRejection',
   'uncaughtException'
@@ -18,5 +21,13 @@ client.listentoProcessEvents([
 ["Reminder", "checkQuests", "Modules"].forEach((functions) => {
 	require(`./functions/${functions}`)(client);
 });
+ 
+server.all('/', (req, res) => {
+  res.send(`OK`)
+})
+ 
+function keepAlive() {
+  server.listen(3000, () => { console.log("Alive") });
+}
 
 client.login(process.env.TOKEN);
