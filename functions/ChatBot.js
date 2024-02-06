d80b93d272495ef0d86b0275084381f7e204972c
BuildTools <unconfigured@null.spigotmc.org>
2024-02-06 19:31:02 +0200

    Fix formatting and uncomment code in ChatBot.js

--- functions/ChatBot.js
+++ functions/ChatBot.js
@@ -1,15 +1,18 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
-const { ChannelType } = require('discord.js')
-const { OpenAIApi, Configuration } = require("openai")
+const { ChannelType } = require("discord.js");
+
+/*
+const { OpenAIApi, Configuration } = require("openai");
 
 const config = new Configuration({
-    apiKey: "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"
-})
+  apiKey: "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",
+});
 
-const openai = new OpenAIApi(config)
+const openai = new OpenAIApi(config);
+*/
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 exports.chat = async function (client, message) {
   if (!message) {
@@ -20,34 +23,18 @@ exports.chat = async function (client, message) {
 
   if (message.channel.type === ChannelType.DM) return;
   if (message.author == client.user) return;
-  if (message.author.bot){
-      return;
-    };
+  if (message.author.bot) {
+    return;
+  }
   //if(message.embeds[0]) return;
 
-  const Channel = message.guild.channels.cache.get(client.config.channels.chatbot)
+  const Channel = message.guild.channels.cache.get(
+    client.config.channels.chatbot
+  );
 
-    if(message.channel.id == Channel?.id) {
-        message.channel.sendTyping()
-        /*
-        const url = 'https://waifu.p.rapidapi.com/v1/waifu';
-
-        const options = {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': 'b335761898msh3524b71f87e82dbp1956dfjsn3ad103632169',
-            'X-RapidAPI-Host': 'waifu.p.rapidapi.com'
-          },
-          body: `{"user_id":"${message.author.id}","message":"${message.content}","from_name":"${message.author.username}","to_name":"${client.user.username}","situation":"${client.user.username} and ${message.author.username} talking in discord, ${client.user.username} loves ${message.author.username}, No bad words.","translate_from":"auto","translate_to":"auto"}`
-        };
-        
-        fetch(url, options)
-          .then(res => res.json())
-          .then(json => message.reply(json.response).catch(() => null))
-          .catch(() => message.reply('Sorry, but something went wrong while responding to this message!').catch(() => null));
-          */
-
+  if (message.channel.id == Channel?.id) {
+    message.channel.sendTyping();
+    /*
           const maxRetries = 3; // Maximum number of retries
           let retryCount = 0;
           let response = null;
@@ -106,6 +93,29 @@ exports.chat = async function (client, message) {
             // Handle failure case
             await message.reply("An error occurred while processing the request.");
           }
-}
+          */
 
+    const url = "https://waifu.p.rapidapi.com/v1/waifu";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "b335761898msh3524b71f87e82dbp1956dfjsn3ad103632169",
+        "X-RapidAPI-Host": "waifu.p.rapidapi.com",
+      },
+      body: `{"user_id":"${message.author.id}","message":"${message.content}","from_name":"${message.author.username}","to_name":"${client.user.username}","situation":"${client.user.username} and ${message.author.username} chatting in discord, ${client.user.username} loves ${message.author.username}, No bad words, clean chat.","translate_from":"auto","translate_to":"auto"}`,
+    };
+
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => message.reply(json.response).catch(() => null))
+      .catch(() =>
+        message
+          .reply(
+            "Sorry, but something went wrong while responding to this message!"
+          )
+          .catch(() => null)
+      );
+  }
 };
