83f7bf975a0fef61d31bd870129bd7c623e82755
iiBlackwolf <blackwolf3466@gmail.com>
2024-03-15 03:09:01 +0200

    Add set-avatar slash command

---
+++ slashCommands/Private/set-avatar.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("set-avatar")
+    .setDescription("🛠 Developer only, set's the avatar for the bot")
+    .addAttachmentOption((option) =>
+      option.setName("avatar").setDescription("The avatar to set!").setRequired(true)
+    ),
+  async execute(client, interaction) {
+    const attachment = interaction.options.getAttachment("avatar");
+
+    return await interaction.editReply({ content: `✨ **Setting** new avatar...` })
+        .then(async () => {
+            return await client.user
+                .setAvatar(attachment.url)
+                .then(() =>
+                    interaction.editReply(`\\✅ New avatar **set**!`).catch(() => null)
+                )
+                .catch((err) => {
+                    console.log(err);
+                    return interaction.editReply(
+                        `❌ Failed to set new avatar! [\`${err.name}\`]`
+                    );
+                });
+        });
+  },
+};
