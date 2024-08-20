61ecbe1335b2f6198db0623fecb558147d6e09ab
WOLF <blackwolf3466@gmail.com>
2024-08-20 10:56:35 +0300

    sameSite: none

--- server/server.js
+++ server/server.js
@@ -49,8 +49,8 @@ module.exports = (client) => {
         }),
         cookie: {
             maxAge: 14 * 24 * 60 * 60 * 1000,
-/*             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'none', */
+            /* secure: process.env.NODE_ENV === 'production', */
+            sameSite: 'none',
         },
     }));
 
