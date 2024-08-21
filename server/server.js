7d446e1a5a04e2a320043c4c5f530df6abf48ddf
WOLF <blackwolf3466@gmail.com>
2024-08-21 09:19:28 +0300

    Trust proxy set to true

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ module.exports = (client) => {
     }
 
     if (app.get('env') === 'production') {
-        app.set('trust proxy', 1);
+        app.set('trust proxy', true);
         sessionOptions.cookie.secure = true;
         sessionOptions.cookie.sameSite = 'none';
     }
