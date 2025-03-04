f13f3be38f974e936817c90f605a49007c5d27c2
WOLF <blackwolf3466@gmail.com>
2025-03-04 06:47:29 +0200

    Logs final touch to be out of beta

--- slashCommands/setup/logs.js
+++ slashCommands/setup/logs.js
@@ -1,7 +1,9 @@
 const {
     EmbedBuilder,
     ChannelType,
-    ApplicationCommandOptionType
+    ApplicationCommandOptionType,
+    ButtonBuilder,
+    ActionRowBuilder
 } = require("discord.js");
 const { ErrorEmbed, SuccessEmbed, InfoEmbed } = require("../../util/modules/embeds");
 const schema = require('../../schema/GuildSchema');
@@ -38,8 +40,8 @@ module.exports = {
             },
             {
                 type: ApplicationCommandOptionType.Subcommand,
-                name: "set-all",
-                description: "Set's the logs for all events in the server",
+                name: "set-global",
+                description: "Set's the logs for global logs(all events) in the server",
                 options: [
                     {
                         type: ApplicationCommandOptionType.Channel,
@@ -67,18 +69,16 @@ module.exports = {
                     },
                     {
                         type: ApplicationCommandOptionType.Boolean,
-                        name: "global-all-status",
-                        description: "Enable or disable global logs",
-                        required: false,
-                    },
-                    {
-                        type: ApplicationCommandOptionType.Channel,
-                        name: "log-channel-status",
-                        description: "Toggle the status of a specific separated logs type by selecting its channel",
-                        channelTypes: [ChannelType.GuildText],
+                        name: "global-status",
+                        description: "Enable or disable global(all events) logs",
                         required: false,
                     }
                 ]
+            },
+            {
+                type: ApplicationCommandOptionType.Subcommand,
+                name: "manage-separated",
+                description: "Sent separated logs configuration panel",
             },
             {
                 type: ApplicationCommandOptionType.Subcommand,
@@ -105,6 +105,9 @@ module.exports = {
                     break;
                 case "list":
                     await listLogs(client, interaction);
+                    break;
+                case "manage-separated":
+                    await manageSeparatedLogs(client, interaction);
                     break;
                 default:
                     await interaction.reply({ embeds: [ErrorEmbed("❌ Invalid subcommand!")], ephemeral: true });
@@ -174,8 +177,8 @@ async function setAllLogs(client, interaction, options) {
                 !data.Mod.Logs.isEnabled ? "⚠️ Logs channel is disabled! To enable, execute `/logs edit all-status [true]`" : null,
                 data.Mod.Logs.type != "all" ? `⚠️ Logs type is set to \`${data.Mod.Logs.type}\`! To change type, execute \`/logs edit logs-type [all]\`` : null
             ]
-            .filter(line => line !== null) // Filter out null values
-            .join("\n") // Join the remaining lines into a single string
+                .filter(line => line !== null) // Filter out null values
+                .join("\n") // Join the remaining lines into a single string
         )],
         ephemeral: true
     });
@@ -270,19 +273,76 @@ async function listLogs(client, interaction) {
             { name: "Logs Enabled", value: logs.isEnabled ? "Yes" : "No", inline: true },
             { name: "Logs Type", value: logs.type || "Not set", inline: true },
             { name: "All Logs Channel", value: logs.channel ? `<#${logs.channel}>` : "Not set", inline: true },
-            { name: "Separated Logs", value: "See below for details", inline: false }
+            { name: "Separated Logs", value: "See below for details", inline: false },
+            ...await separatedList(logs)
         );
-
-    // Dynamically add fields for each log type
-    for (const [key, logType] of Object.entries(logTypes)) {
-        const log = logs.separated?.[key];
-        const value = log?.channel ? `<#${log.channel}> (\`${log.isEnabled ? "Enabled" : "Disabled"}\`)` : "Not set";
-        embed.addFields({ name: logType.name, value, inline: true });
-    }
-
-    // Add a timestamp
-    embed.setTimestamp();
 
     // Send the embed
     await interaction.reply({ embeds: [embed], ephemeral: true });
+}
+async function manageSeparatedLogs(client, interaction, options) {
+    const { guild } = interaction;
+    const logData = await schema.findOne({ GuildID: guild.id });
+
+    if (!logData || !logData.Mod?.Logs) {
+        return interaction.reply({ embeds: [ErrorEmbed("❌ No logs configuration found for this server.")], ephemeral: true });
+    }
+
+    const logs = logData.Mod.Logs;
+
+    // Create the base embed
+    const embed = new EmbedBuilder()
+        .setTitle("Separated Logs Configuration")
+        .setDescription(["<a:Fix:1267280059517894737> Manage separated logs events using **configurations** below", "Toggle events **status** by pressing their button"].join("\n"))
+        .addFields(await separatedList(logs));
+
+    const msg = await interaction.reply({ embeds: [embed], components: await createButtonsAndRows(logs), fetch: true });
+    const collector = msg.createMessageComponentCollector({ time: 860000, errors: ['time'] });
+
+    collector.on('collect', async (button) => {
+        const logType = button.customId.split('-')[1];
+        const log = logs.separated?.[logType];
+
+        if (!log) {
+            return button.reply({ content: "This log type is not set.", ephemeral: true });
+        }
+
+        log.isEnabled = !log.isEnabled;
+
+        await schema.findOneAndUpdate({ GuildID: guild.id }, { $set: { [`Mod.Logs.separated.${logType}.isEnabled`]: log.isEnabled } });
+
+        await msg.edit({ embeds: [embed.setFields(await separatedList(logs))], components: await createButtonsAndRows(logs) });
+        button.reply({
+            embeds: [InfoEmbed(`${logType} logs has been set to **${log.isEnabled ? "Enabled" : "Disabled"}**.`)],
+            ephemeral: true
+        });
+    });
+}
+async function createButtonsAndRows(logs) {
+    let buttons = [];
+
+    for (const [key, logType] of Object.entries(logTypes)) {
+        const log = logs.separated?.[key];
+        buttons.push(new ButtonBuilder().setLabel(logType.name).setCustomId(`toggle-${key}`).setStyle(log?.isEnabled ? "Success" : "Danger").setDisabled(!log?.channel));
+    }
+
+    let rows = [];
+
+    for (let i = 0; i < buttons.length; i += 5) {
+        rows.push(new ActionRowBuilder().addComponents(buttons.slice(i, i + 5)));
+    }
+
+    return rows;
+
+}
+async function separatedList(logs) {
+    const fields = [];
+
+    for (const [key, logType] of Object.entries(logTypes)) {
+        const log = logs.separated?.[key];
+        const value = log?.channel ? `<#${log.channel}> (\`${log.isEnabled ? "Enabled" : "Disabled"}\`)` : "Not set";
+        fields.push({ name: logType.name, value, inline: true });
+    }
+
+    return fields;
 }
